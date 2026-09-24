a2d677fc61e12804b463e41021dcb09eb8eab3b7
Nicolaskn95 <nicolasjap@hotmail.com>
2026-09-24 20:32:55 -0300

    feat: add root layout with metadata, fonts, and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,15 @@ export default function RootLayout({
             gtag('config', 'G-KRPRVERCJW');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yn4j4garoo");
+          `}
+        </Script>
       </body>
     </html>
   )
